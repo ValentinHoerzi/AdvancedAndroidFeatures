b5f70502f5a83aa1b81a0bf8d350d2923ed3142a
Valentin Hörzi <valentin.hoerzi@gmail.com>
2019-03-20 17:12:10 +0100

    init commit

---
+++ app/src/main/java/vhoerzi16/main/MainActivity.java
@@ -0,0 +1,16 @@
+package vhoerzi16.main;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
